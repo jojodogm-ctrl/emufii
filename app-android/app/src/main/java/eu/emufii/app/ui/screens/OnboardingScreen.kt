ce1fdb95b6dcdedb4e40ffe80e0a92f7d44ccbea
Brian Jr <thaballa79@gmail.com>
2026-09-02 20:25:42 -0400

    clears unused imports

--- app-android/app/src/main/java/eu/emufii/app/ui/screens/OnboardingScreen.kt
+++ app-android/app/src/main/java/eu/emufii/app/ui/screens/OnboardingScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -35,7 +34,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -74,7 +72,6 @@ import eu.emufii.app.ui.components.SignalMark
 import eu.emufii.app.ui.components.SoftCard
 import eu.emufii.app.ui.components.SteamGridDbMark
 import eu.emufii.app.ui.components.waitTrim
-import eu.emufii.app.ui.controlRing
 import eu.emufii.app.ui.screens.settings.AutofillBlock
 import eu.emufii.app.ui.screens.settings.BlockFact
 import eu.emufii.app.ui.screens.settings.BlockNotice
